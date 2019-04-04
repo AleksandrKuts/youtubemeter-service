6850c83e37c130990a962246620c0c3ef8f38666
Aleksandr Kuts <kr110666kai@gmail.com>
2019-04-04 09:01:01 +0300

    fix

--- backend/cmd_backend/main.go
+++ backend/cmd_backend/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"github.com/AleksandrKuts/youtubemeter-service/backend"
 )
-const versionMajor = "1.0"
+const versionMajor = "2.0"
 
 var (
 	version string
--- sql/create_db.sh
+++ sql/create_db.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+sudo -u postgres createuser -P youtubemetric
 sudo -u postgres createdb -O youtubemetric -E Unicode -T template0 youtube_statistics
 cat schema.sql | sudo -u postgres psql -U youtubemetric youtube_statistics
